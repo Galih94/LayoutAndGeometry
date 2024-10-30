b41f8dc1c847c021ac1370c623f5584b8f8e5018
galih <samudragalih94@gmail.com>
2024-10-30 11:17:21 +0700

    feat(position): use position to use whole screen

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello world")
+            .background(.red)
+            .position(x: 100, y: 100)
+            .background(.blue)
     }
 }
 
