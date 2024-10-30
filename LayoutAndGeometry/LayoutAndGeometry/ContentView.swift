4de2dbc8c3f11d6449ccce499b3305fb069f1cff
galih <samudragalih94@gmail.com>
2024-10-30 11:07:54 +0700

    clean code

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,20 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            ForEach(0..<10) { position in
-                Text("number \(position)")
-                    .alignmentGuide(.leading, computeValue: { _ in
-                        Double(position) * -10
-                    })
-                
-            }
-        }
-        .background(.red)
-        .frame(width: 400, height: 400)
-        .background(.blue)
-               
-        
+        Text("test")
     }
 }
 
