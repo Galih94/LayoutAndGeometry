650576d4de3c30704ae8b8d9e19066b2201d7cd8
galih <samudragalih94@gmail.com>
2024-10-30 11:11:36 +0700

    feat(Custom Alignment): create Custom Alignment to make Text("GALIH SAMODRA") and Text("@gsamudra") align center

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,10 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("test")
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@gsamudra")
+                    .alignmentGuide(.midAccountAndName, computeValue: { dimension in
+                        dimension[VerticalAlignment.center]
+                    })
+                Image(.default)
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+            
+            VStack {
+                Text("Full name:")
+                Text("GALIH SAMODRA")
+                    .alignmentGuide(.midAccountAndName, computeValue: { dimension in
+                        dimension[VerticalAlignment.center]
+                    })
+                    .font(.largeTitle)
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+extension VerticalAlignment {
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+    enum MidAccountAndName: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[.top]
+        }
+    }
 }
