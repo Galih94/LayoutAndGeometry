f61bf76035c3dd6fe7c415b944f4abddad5c1ce5
galih <samudragalih94@gmail.com>
2024-10-30 09:07:48 +0700

    wip initial project

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Galih Samudra on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
