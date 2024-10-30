31f810554db75a6a03c4204131306729a4bf36f0
galih <samudragalih94@gmail.com>
2024-10-30 11:13:32 +0700

    clean code

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,39 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@gsamudra")
-                    .alignmentGuide(.midAccountAndName, computeValue: { dimension in
-                        dimension[VerticalAlignment.center]
-                    })
-                Image(.default)
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-            
-            VStack {
-                Text("Full name:")
-                Text("GALIH SAMODRA")
-                    .alignmentGuide(.midAccountAndName, computeValue: { dimension in
-                        dimension[VerticalAlignment.center]
-                    })
-                    .font(.largeTitle)
-            }
-        }
+        Text("Hello world")
     }
 }
 
 #Preview {
     ContentView()
-}
-
-
-extension VerticalAlignment {
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
-    enum MidAccountAndName: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[.top]
-        }
-    }
 }
