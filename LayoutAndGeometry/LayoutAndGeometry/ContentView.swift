cd28d6508765f96c9895f3b6dc63493f3140c250
galih <samudragalih94@gmail.com>
2024-10-31 12:51:12 +0700

    feat(GeometryReader): apply a more complex rotation3DEffect() that subtracts half the height of the main view, but that means using a second GeometryReader to get the size of the main view, the views will reach a natural orientation nearer the center of the screen

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -10,20 +10,19 @@ import SwiftUI
 struct ContentView: View {
     let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
     var body: some View {
-        ScrollView {
-            ForEach(0..<50) { index in
-                GeometryReader { proxy in
-                    Text("row \(index)")
-                        .font(.title)
-                        .frame(maxWidth: .infinity)
-                        .background(colors[index % 7])
-                        .rotation3DEffect(
-                            .degrees(proxy.frame(in: .global).minY / 5),
-                            axis: (x: 0.0, y: 1.0, z: 0.0)
-                        )
+        GeometryReader { fullView in
+            ScrollView {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        Text("row \(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0.0, y: 1.0, z: 0.0))
+                    }
+                    .frame(height: 40)
+                    
                 }
-                .frame(height: 40)
-                
             }
         }
     }
