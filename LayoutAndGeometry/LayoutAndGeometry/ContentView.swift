c4c5e4dc2541f88918903307c580afc137d15259
galih <samudragalih94@gmail.com>
2024-10-31 12:41:37 +0700

    feat(Helix): helix-style spinning effect, using rotation3DEffect()

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
     var body: some View {
-        Text("Hello world")
+        ScrollView {
+            ForEach(0..<50) { index in
+                GeometryReader { proxy in
+                    Text("row \(index)")
+                        .font(.title)
+                        .frame(maxWidth: .infinity)
+                        .background(colors[index % 7])
+                        .rotation3DEffect(
+                            .degrees(proxy.frame(in: .global).minY / 5),
+                            axis: (x: 0.0, y: 1.0, z: 0.0)
+                        )
+                }
+                .frame(height: 40)
+                
+            }
+        }
     }
 }
 
