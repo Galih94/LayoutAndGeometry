6f34598ff99f55c5b9769fa0e5b23f151118c66e
galih <samudragalih94@gmail.com>
2024-10-31 16:39:35 +0700

    feat(CHALLANGE): Make views adjust their scale depending on their vertical position, with views near the bottom being large and views near the top being small. I would suggest going no smaller than 50% of the regular size

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,18 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
-
+    
     var body: some View {
         GeometryReader { fullView in
             ScrollView(.vertical) {
                 ForEach(0..<50) { index in
                     GeometryReader { proxy in
-                        Text("Row #\(index)")
+                        Text("Row #\(index) \(proxy.frame(in: .global).minY)")
+                            .frame(height: calculateHeight(for: Double(proxy.frame(in: .global).minY)))
                             .font(.title)
                             .frame(maxWidth: .infinity)
                             .background(colors[index % 7])
                             .opacity(calculateOpacity(at: Double(proxy.frame(in: .global).minY)))
                             .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            
                     }
                     .frame(height: 40)
                 }
@@ -35,6 +37,16 @@ struct ContentView: View {
             return (pos - 60) / 140
         } else {
             return 1 + (pos - 200) / 100
+        }
+    }
+    
+    private func calculateHeight(for pos: Double) -> Double {
+        if pos <= 120 {
+            return 20
+        } else if pos < 400 {
+            return 20 + (pos - 120) * 20 / 280 
+        } else {
+            return 40
         }
     }
 }
