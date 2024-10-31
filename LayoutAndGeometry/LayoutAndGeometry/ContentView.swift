23c9f522f5f96b0d375f69ffa7a49a5c3fef4b40
galih <samudragalih94@gmail.com>
2024-10-31 09:00:59 +0700

    feat(containerRelativeFrame): use containerRelativeFrame to set 80% size

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        GeometryReader { proxy in
-            Image(.default)
-                .resizable()
-                .scaledToFit()
-                .frame(width: proxy.size.width * 0.8, height: 300)
-        }
+        Image(.default)
+            .resizable()
+            .scaledToFit()
+            .containerRelativeFrame(.horizontal, { size, axis in
+                size * 0.8
+            })
+            .background(.green)
     }
 }
 
