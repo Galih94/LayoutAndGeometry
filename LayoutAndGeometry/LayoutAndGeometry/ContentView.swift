20e30695b0d7e4b4c9e7df15d7faefda55692662
galih <samudragalih94@gmail.com>
2024-10-31 09:19:22 +0700

    clean code

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,20 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            Text("IMPORTANT")
-                .frame(width: 200)
-                .background(.blue)
-            
-            GeometryReader { proxy in
-                Image(.default)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: proxy.size.width * 0.8)
-                    .frame(width: proxy.size.width, height: proxy.size.height)
-                    .background(.green)
-            }
-        }
+        Text("Hello world")
     }
 }
 
