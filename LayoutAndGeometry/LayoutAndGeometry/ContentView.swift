e3bbe0f649c0f3b4fb462ab7ae59ab57f3b231e6
galih <samudragalih94@gmail.com>
2024-10-31 13:21:13 +0700

    feat(visualEffect): replace GeometryReader by visualEffect to create same CoverFlow-style

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -13,16 +13,14 @@ struct ContentView: View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 0) {
                 ForEach(0..<50) { index in
-                    GeometryReader { proxy in
-                        Text("Number \(index)")
-                            .font(.largeTitle)
-                            .padding()
-                            .background(.red)
-                            .rotation3DEffect(.degrees(-proxy.frame(in: .global).minX) / 8, axis: (x: 0.0, y: 1.0, z: 0.0))
-                            .frame(width: 200, height: 200)
-                    }
-                    .frame(width: 200, height: 200)
-                    
+                    Text("Number \(index)")
+                        .font(.largeTitle)
+                        .padding()
+                        .background(.red)
+                        .frame(width: 200, height: 200)
+                        .visualEffect { content, proxy in
+                            content.rotation3DEffect(.degrees(-proxy.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
+                        }
                 }
             }
         }
